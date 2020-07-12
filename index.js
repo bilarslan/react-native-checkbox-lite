adca63e7bb5a3299177cefc8dd01416c341a73df
bilarslan <bilarslan@windowslive.com>
2020-07-12 16:53:26 +0300

    export default function

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ import { Text, Image, TouchableOpacity } from 'react-native';
 const CHECKBOX_MARKED = require('./static/checkbox-marked.png');
 const CHECKBOX_BLANK_OUTLINE = require('./static/checkbox-blank-outline.png');
 
-const CheckBox = ({ style, checkBoxSize = 24, checkBoxColor = '#000', isChecked = false, onPress, text = 'Checkbox Text', textStyle }) => {
+export default function CheckBox({ style, checkBoxSize = 24, checkBoxColor = '#000', isChecked = false, onPress, text = 'Checkbox Text', textStyle }) {
     return (
         <TouchableOpacity style={[{ flexDirection: 'row', alignItems: 'center' }, style]} onPress={onPress}>
             <Image style={{ width: checkBoxSize, height: checkBoxSize, tintColor: checkBoxColor }} source={isChecked ? CHECKBOX_MARKED : CHECKBOX_BLANK_OUTLINE} />
@@ -12,5 +12,3 @@ const CheckBox = ({ style, checkBoxSize = 24, checkBoxColor = '#000', isChecked 
         </TouchableOpacity>
     );
 }
-
-export { CheckBox };
